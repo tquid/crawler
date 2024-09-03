2312623c60b62c73b9a4306dbb5881e632f0ec8a
JB Bell <tquidca@gmail.com>
2024-09-03 10:38:20 -0700

    Now with testing

---
+++ normalize_url.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+func normalizePath(path string) (string, error) {
+	c := []rune(path)
+	if c[len(c)-1] == '/' {
+		return string(c[:len(c)-1]), nil
+	}
+	return path, nil
+}
+
+func normalizeURL(urlStr string) (string, error) {
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		return "", fmt.Errorf("could not parse url: %v", urlStr)
+	}
+	normalizedPath, err := normalizePath(u.Path)
+	if err != nil {
+		return "", fmt.Errorf("could not normalize path '%v'", u.Path)
+	}
+	normalized := u.Host + normalizedPath
+	return normalized, nil
+}
---
+++ normalize_url_test.go
@@ -0,0 +1,40 @@
+package main
+
+import "testing"
+
+func TestNormalizeURL(t *testing.T) {
+	tests := []struct {
+		name     string
+		inputURL string
+		expected string
+	}{
+		{
+			name:     "remove scheme",
+			inputURL: "https://blog.boot.dev/path",
+			expected: "blog.boot.dev/path",
+		},
+		{
+			name:     "remove terminal slash",
+			inputURL: "https://blog.boot.dev/path/",
+			expected: "blog.boot.dev/path",
+		},
+		{
+			name:     "exclude user:password string",
+			inputURL: "https://foo:bar@blog.boot.dev/path",
+			expected: "blog.boot.dev/path",
+		},
+	}
+
+	for i, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := normalizeURL(tc.inputURL)
+			if err != nil {
+				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
+				return
+			}
+			if actual != tc.expected {
+				t.Errorf("Test %v - %s FAIL: expected URL: %v, actual: %v", i, tc.name, tc.expected, actual)
+			}
+		})
+	}
+}
