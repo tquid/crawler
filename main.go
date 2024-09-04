66e9d706a82cb697cb63852dbcf481eec7bb27b5
JB Bell <tquidca@gmail.com>
2024-09-04 09:04:50 -0700

    Start of CLI features

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+	if len(args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	fmt.Printf("starting crawl of: %s\n", os.Args[1])
 }
