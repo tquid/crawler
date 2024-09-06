8886549ae5dcb1df0ed19b9518f6487829a3f442
JB Bell <tquidca@gmail.com>
2024-09-06 12:56:31 -0700

    Use html.Parse instead of tokenizer

--- get_urls_from_html.go
+++ get_urls_from_html.go
@@ -2,49 +2,47 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/url"
 	"strings"
 
 	"golang.org/x/net/html"
 )
 
-func isPathRelative(urlStr string) (bool, error) {
-	parsedURL, err := url.Parse(urlStr)
-	if err != nil {
-		return false, fmt.Errorf("can't parse url '%v'", urlStr)
-	}
-	if parsedURL.Scheme == "" && parsedURL.Host == "" {
-		return true, nil
-	}
-	return false, nil
-}
-
 func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
-	urls := []string{}
-	z := html.NewTokenizer(strings.NewReader(htmlBody))
-	for {
-		tt := z.Next()
-		switch tt {
-		case html.ErrorToken:
-			if z.Err() == io.EOF {
-				return urls, nil
-			}
-			return nil, z.Err()
-		case html.StartTagToken:
-			t := z.Token()
-			if t.Data == "a" {
-				for _, a := range t.Attr {
-					if a.Key == "href" {
-						if r, _ := isPathRelative(a.Val); r {
-							urls = append(urls, rawBaseURL+a.Val)
-						} else {
-							urls = append(urls, a.Val)
-						}
-						break
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse base URL: %v", err)
+	}
+
+	htmlReader := strings.NewReader(htmlBody)
+	doc, err := html.Parse(htmlReader)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse HTML: %v", err)
+	}
+
+	var urls []string
+	var traverseNodes func(*html.Node)
+	traverseNodes = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "a" {
+			for _, anchor := range node.Attr {
+				if anchor.Key == "href" {
+					href, err := url.Parse(anchor.Val)
+					if err != nil {
+						fmt.Printf("couldn't parse href '%v': %v\n", anchor.Val, err)
+						continue
 					}
+
+					resolvedURL := baseURL.ResolveReference(href)
+					urls = append(urls, resolvedURL.String())
 				}
 			}
 		}
+
+		for child := node.FirstChild; child != nil; child = child.NextSibling {
+			traverseNodes(child)
+		}
 	}
+	traverseNodes(doc)
+
+	return urls, nil
 }
